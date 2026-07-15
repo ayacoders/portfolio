5b75f85173c8e284714928a1f524311c0195e453
Aya <ayacoders@gmail.com>
2026-07-15 22:29:58 +0800

    feat: add contact links data

---
+++ lib/data/contact.ts
@@ -0,0 +1,28 @@
+import { SiGithub, SiGmail } from "@icons-pack/react-simple-icons"
+import type { IconType } from "@icons-pack/react-simple-icons"
+
+import { LinkedinIcon } from "@/components/portfolio/linkedin-icon"
+
+export type ContactLink = {
+  label: string
+  href: string
+  icon: IconType
+}
+
+export const contactLinks: ContactLink[] = [
+  {
+    label: "bandebasethanpatrick25@gmail.com",
+    href: "mailto:bandebasethanpatrick25@gmail.com",
+    icon: SiGmail,
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/ayacoders",
+    icon: SiGithub,
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/ayacoders",
+    icon: LinkedinIcon,
+  },
+]
